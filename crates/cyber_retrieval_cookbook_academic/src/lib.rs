e1dbcfbc0bebd6a61e762ca8928fa82505c31825
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-24 15:46:48 -0700

    Create lib.rs
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

---
+++ crates/cyber_retrieval_cookbook_academic/src/lib.rs
@@ -0,0 +1,13 @@
+#![forbid(unsafe_code)]
+
+pub mod intent;
+pub mod envelope;
+pub mod ksrs;
+pub mod rope;
+pub mod quiz_math;
+
+pub use intent::RetrievalIntent;
+pub use envelope::{PromptEnvelope, Domain, XrZone, AllowedCodeActions};
+pub use ksrs::{KsrTriple, KSR_CEILING_DEFAULT};
+pub use rope::{NeuralRopeId, NeuralRopeSegment, NeuralRope};
+pub use quiz_math::{QuizResult, QuizScore};
